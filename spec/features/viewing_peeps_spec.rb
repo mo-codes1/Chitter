5354984e2f450e0797b0e511f6f605fdf5fab6d6
Mohammed Salah <mohammed_salah@icloud.com>
2021-11-21 13:17:33 +0000

    added functionality to see peeps in chronological order

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,7 +2,7 @@ feature 'viewing peeps' do
   scenario 'the ability to view a peep' do
     connection = PG.connect(dbname: 'chitter_test')
 
-    connection.exec("INSERT INTO peeps VALUES(1, 'Hello Chitter');")
+    connection.exec("INSERT INTO peeps (message) VALUES('Hello Chitter');")
     visit '/peeps'
     expect(page).to have_content 'Hello Chitter' 
   
